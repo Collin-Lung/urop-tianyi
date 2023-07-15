aac38df83956a00b95ce75e6623f98522a05b3b3
Collin Lung <collinlung1@gmail.com>
2023-07-14 20:19:58 -0400

    p5 pca hlb representation with 24 block copolymers

--- PCA/HLB/hlb-pca-with-blockcopolymers.py
+++ PCA/HLB/hlb-pca-with-blockcopolymers.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import PCA
 
 # Opening and cleaning the data
 with open('C:/Users/colli/OneDrive - Massachusetts Institute of Technology/2022-2023 UROP Tianyi/'
-          'Dimensionality Reduction/PCA/p5.txt') as p5:
+          'Dimensionality Reduction/PCA/p5-with-block-copolymers.txt') as p5:
     string_p5 = ''.join(p5)
     new_p5 = string_p5.strip("[]")
     no_comma_p5 = new_p5.replace(',', '')
@@ -26,7 +26,7 @@ for monomer in final_p5:
         HLB_p5.append(monomer.replace('4', '18.5'))
 
 # Fit and transform the data
-x = np.reshape(HLB_p5, (100, 9216)).T
+x = np.reshape(HLB_p5, (100, 9240)).T
 df = pd.DataFrame(x)
 
 # Initialize the PCA model
@@ -36,48 +36,24 @@ p5_pca = pca.fit_transform(df)
 # Explained variance ratio
 per_var = np.round(pca.explained_variance_ratio_ * 100, decimals=1)
 
-# total_var = 0
-# added_per_var = []
-# for var in per_var:
-#     total_var += var
-#     added_per_var.append(total_var)
-#
-# # Plot the explained variance ratio
-# num_line = [i+1 for i in range(len(per_var))]
-# plt.plot(num_line, added_per_var, '-')
-# plt.ylabel('Explained Variance Ratio (%)', fontsize=16)
-# plt.xlabel('Summation of Principal Components', fontsize=16)
-# plt.title('Explained Variance Ratio of Principal Components', fontsize=16)
-# # plt.ylim([0, max(per_var)+90])
-# plt.show()
-
 # Calculate participation ratio of each principal component
 total_var = np.sum(pca.explained_variance_)
 part_ratio = [((i/total_var)**2)/(len(pca.components_)) for i in pca.explained_variance_]
 
-# # Plot the participation ratio
-# num_line = [i+1 for i in range(len(part_ratio))]
-# plt.plot(num_line, part_ratio, '-o')
-# plt.ylabel('Participation Ratio')
-# plt.xlabel('Principal Component')
-# plt.title('Participation Ratio of Principal Components')
-# plt.ylim([0, .001])
-# plt.show()
-
 # Creating a scatter plot of the reduced data
 plt.figure(figsize=(8, 6))
-plt.scatter(p5_pca[:, 0], p5_pca[:, 1], alpha=0.20, s=10)
-plt.title('HLB p5 PCA', fontsize=25)
-plt.xlabel("PC1", fontsize=18)
-plt.ylabel("PC2", fontsize=18)
-plt.xlabel('PC1 - {0}%'.format(per_var[0]), fontsize=18)
-plt.ylabel('PC2 - {0}%'.format(per_var[1]), fontsize=18)
 
 # Labeling sequences in the scatter plot
-seq1_indices_to_label = [9215]
+seq1_indices_to_label = [seq for seq in range(9217, 9241)]
 
 for index, txt in enumerate(p5_pca):
     if index in seq1_indices_to_label:
-        plt.annotate("seq", (p5_pca[index, 0], p5_pca[index, 1]))
+        plt.scatter(p5_pca[index, 0], p5_pca[index, 1], alpha=0.50, s=10, c='red', label='Labeled')
+        plt.annotate(f"{index}", (p5_pca[index, 0], p5_pca[index, 1]))
+    else:
+        plt.scatter(p5_pca[index, 0], p5_pca[index, 1], alpha=0.20, s=10, c='blue', label='Unlabeled')
 
+plt.title('HLB p5 PCA', fontsize=25)
+plt.xlabel("PC1 - {0}%".format(per_var[0]), fontsize=18)
+plt.ylabel("PC2 - {0}%".format(per_var[1]), fontsize=18)
 plt.show()
